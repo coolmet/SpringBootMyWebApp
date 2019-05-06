76c0b3d0f3cb20050eb13752d6c7edd52ea39f7c
Metehan <metehanmeral@hotmail.com>
2019-05-06 21:00:18 +0300

    20190420

--- src/main/java/com/springboot/mywebapp/SprinBootMyWebAppConfiguration.java
+++ src/main/java/com/springboot/mywebapp/SprinBootMyWebAppConfiguration.java
@@ -13,9 +13,9 @@ import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandl
 @Configuration
 public class SprinBootMyWebAppConfiguration
 {
-	
 	@Autowired
 	private SpringBootMyWebAppProperties springBootMyWebAppProperties;
+	
 	@Autowired
 	private RequestMappingHandlerAdapter requestMappingHandlerAdapter;
 	
@@ -23,7 +23,7 @@ public class SprinBootMyWebAppConfiguration
 	public void init()
 	{
 		requestMappingHandlerAdapter.setIgnoreDefaultModelOnRedirect(true);
-			
+		
 		System.out.println(AnsiOutput.toString(AnsiColor.BRIGHT_BLUE,"________________________________________\n",AnsiColor.DEFAULT));
 		System.out.println("\u001b[40m \u001B[38;5;220m @@@@@: SprinBootMyWebAppConfiguration->init()->myPropValue \u001b[0m \u001B[31m"+springBootMyWebAppProperties.isTestBooleanValue()+"\t"+new SimpleDateFormat("dd.MM.yyyy hh:mm:ss.SSS").format(new Date()));
 		System.out.println(AnsiOutput.toString(AnsiColor.BRIGHT_BLUE,"________________________________________",AnsiColor.DEFAULT));
